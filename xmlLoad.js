2d14958948a305ab56b39ecc1e8dff95e3f39744
peytonratliff <ratliffbox@gmail.com>
2021-08-13 01:52:23 -0400

    adding contents

---
+++ xmlLoad.js
@@ -0,0 +1,35 @@
+// window.addEventListener("load", function() {
+// 	getRows();
+// });
+
+// function getRows() {
+//     var xmlhttp = new XMLHttpRequest();
+//     xmlhttp.open("get", "Employees.xml", true);
+// }
+
+// let getXMLFile = function(path, callback){
+//     let request  = new XMLHttpRequest();
+//     request.open("GET", path);
+//     request.setRequestHeader("Content-Type", "text/xml");
+//     request.onreadystatechange = function(){
+//         if(request.readyState === 4 && request.status === 200) {
+//             callback(request.responseXML);
+//         }
+//     };
+//     request.send();
+// };
+
+// getXMLFile("Employees.xml", function(xml) {
+//     console.log(xml);
+// });
+
+
+fetch('https://peytonratliff.github.io/Employees.xml')
+    .then(function(resp){
+        return resp.text();
+    })
+    .then(function(data){
+        let parser = new DOMParser(),
+            xmlDoc = parser.parseFromString(data, 'text/xml');
+        console.log(xmlDoc.getElementsByTagName('Employee'));
+    });
