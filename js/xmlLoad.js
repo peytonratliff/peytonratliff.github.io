75e19c370db708f68f0e68a161e1689be3af1da2
Peyton Ratliff <ratliffbox@gmail.com>
2021-08-13 15:04:23 -0400

    changes

--- js/xmlLoad.js
+++ js/xmlLoad.js
@@ -1,29 +1,3 @@
-// window.addEventListener("load", function() {
-// 	getRows();
-// });
-
-// function getRows() {
-//     var xmlhttp = new XMLHttpRequest();
-//     xmlhttp.open("get", "Employees.xml", true);
-// }
-
-// let getXMLFile = function(path, callback){
-//     let request  = new XMLHttpRequest();
-//     request.open("GET", path);
-//     request.setRequestHeader("Content-Type", "text/xml");
-//     request.onreadystatechange = function(){
-//         if(request.readyState === 4 && request.status === 200) {
-//             callback(request.responseXML);
-//         }
-//     };
-//     request.send();
-// };
-
-// getXMLFile("Employees.xml", function(xml) {
-//     console.log(xml);
-// });
-
-
 fetch('https://peytonratliff.github.io/classicWoWxml/druid.xml')
     .then(function(resp){
         return resp.text();
@@ -32,4 +6,8 @@ fetch('https://peytonratliff.github.io/classicWoWxml/druid.xml')
         let parser = new DOMParser(),
             xmlDoc = parser.parseFromString(data, 'text/xml');
         console.log(xmlDoc.getElementsByTagName('talentName'));
+
+        var row1col1 = document.getElementById(id="row1col1");
+        row1col1.innerHTML = xmlDoc.firstChild;
     });
+
