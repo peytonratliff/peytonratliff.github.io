c6e0c4866dfca5c8d34222a2dadaabcc91fc12de
peytonratliff <ratliffbox@gmail.com>
2021-08-14 22:51:23 -0400

    index update
    <3 alex

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 function showToad(){
     var toad = document.getElementById("toad");
+    var noButton = document.getElementById("noButton");
+
+
     toad.style.display = "block";
+    noButton.style.display = "block";
+}
+
+function noToad(){
+    var toad = document.getElementById("toad");
+    var noButton = document.getElementById("noButton");
+
+    toad.style.display = "none";
+    noButton.style.display = "none";
 }
