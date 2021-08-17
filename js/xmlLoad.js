fa6513b662ea2931cd3d388983020826a9ba8e7e
peytonratliff <ratliffbox@gmail.com>
2021-08-17 14:45:25 -0400

    fetch different xml url based on id
    each button passes an id (the name of the class it represents) and the url for the xml is based off of the passed on that id

--- js/xmlLoad.js
+++ js/xmlLoad.js
@@ -1,11 +1,11 @@
-function xmlLoad(){
-    fetch('https://peytonratliff.github.io/classicWoWxml/druid.xml')
+function xmlLoad(classID){
+    fetch('https://peytonratliff.github.io/classicWoWxml/' + classID + ".xml")
     .then(function(resp){
         return resp.text();
     })
     .then(function(data){
         let parser = new DOMParser(),
             xmlDoc = parser.parseFromString(data, 'text/xml');
-        console.log(xmlDoc.getElementsByTagName('druid'));
+        console.log(xmlDoc.getElementsByTagName(classID));
     });
 }
