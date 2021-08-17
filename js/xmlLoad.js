1cc4104e3d7969cb16a4734933202b617d7e8b48
peytonratliff <ratliffbox@gmail.com>
2021-08-17 14:36:18 -0400

    workin

--- js/xmlLoad.js
+++ js/xmlLoad.js
@@ -1,13 +1,11 @@
-fetch('https://peytonratliff.github.io/classicWoWxml/druid.xml')
+function xmlLoad(){
+    fetch('https://peytonratliff.github.io/classicWoWxml/druid.xml')
     .then(function(resp){
         return resp.text();
     })
     .then(function(data){
         let parser = new DOMParser(),
             xmlDoc = parser.parseFromString(data, 'text/xml');
-        console.log(xmlDoc.getElementsByTagName('talentName'));
-
-        var row1col1 = document.getElementById(id="row1col1");
-        row1col1.innerHTML = xmlDoc.firstChild;
+        console.log(xmlDoc.getElementsByTagName('druid'));
     });
-
+}
